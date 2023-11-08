7192892fca78d5492954fce2e1bdf25e56a07015
David <dave.pond@gmail.com>
2023-11-08 12:09:25 -0500

    Fine tuned ball movement/bouncing

--- Ball/Ball.cs
+++ Ball/Ball.cs
@@ -3,18 +3,22 @@ using System;
 
 public partial class Ball : CharacterBody2D
 {
-	[Export] private int MoveSpeed = 50;
+	[Export] private float MoveSpeed = 50;
 	public Vector2 Movement;
 
 	public override void _Ready()
 	{
-		Movement = new Vector2(1, 1);
-		Velocity = Movement * MoveSpeed;
+		Velocity = Vector2.Down * MoveSpeed;
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		HandleCollision(MoveAndSlide());
+	}
+
+	public void Launch()
+	{
+		Velocity = new Vector2(0, -1).Normalized() * MoveSpeed;
 	}
 
 	private void HandleCollision(bool isColliding)
@@ -26,9 +30,19 @@ public partial class Ball : CharacterBody2D
 		var collider = GetSlideCollision(0).GetCollider();
 		
 		if (collider is Brick brick)
+		{
 			brick.Hit();
+			MoveSpeed *= 1.05f;
+		}
 		
-		var travel = GetSlideCollision(0).GetTravel().Normalized();
+		var travel = GetSlideCollision(0).GetTravel();
+
+		// add some variance to the ball trajectory depending on where the ball hits the paddle
+		// more variance added the farther away from the center of the paddle that the ball hits
+		if (collider is Paddle paddle)
+			travel.X -= GlobalPosition.DirectionTo(paddle.GlobalPosition).X * 0.15f;
+
+		travel = travel.Normalized();
 
 		if (IsOnFloor())
 			Velocity = new Vector2(travel.X, -travel.Y) * MoveSpeed;
