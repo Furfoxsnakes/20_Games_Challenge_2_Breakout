08cd74b67bd2089ff6979aa15e041ea70d5b8daf
David <dave.pond@gmail.com>
2023-11-08 12:09:25 -0500

    Fine tuned ball movement/bouncing

--- Game.cs
+++ Game.cs
@@ -3,8 +3,17 @@ using System;
 
 public partial class Game : Node
 {
+    
+    
     public override void _Ready()
     {
         // Input.MouseMode = Input.MouseModeEnum.Hidden;
+    }
+    
+    public enum GameState
+    {
+        Playing,
+        GameOver,
+        WaitingForLaunch
     }
 }
